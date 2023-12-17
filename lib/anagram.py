42d0a5b779ea4ef7e4ec1174983f78c2d79b1276
Christopher <chrisjwkim97@gmail.com>
2023-12-17 15:45:02 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, name):
+        self.name = name
+
+    def match(self, list):
+        anagrams = []
+        name = []
+        for char in self.name:
+            name.append(char)
+        
+        sortedName = sorted(name)
+
+        for word in list:
+            letters = []
+            for char in word:
+                letters.append(char)
+            sortedLetters = sorted(letters)
+            if sortedLetters == sortedName:
+                anagrams.append(word)
+
+        return anagrams
